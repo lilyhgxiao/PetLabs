907fc1f1e579c21d8819fa49e6a6c05ef55912e5
cherkaski <alexcher81@gmail.com>
2019-10-21 12:57:09 -0400

    Add empty React components for Admin list pages

---
+++ frontend/petlabsfrontend/src/Components/AdminPetListPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class AdminPetListPage extends React.Component {
+    render() {
+        return(
+            <div>AdminPetsListPage</div>
+        );
+    }
+}
+
+export default AdminPetListPage;
