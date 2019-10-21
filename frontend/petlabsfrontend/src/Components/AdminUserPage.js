6c3b54800d595e99257f1d34cee80a45425ea6a0
cherkaski <alexcher81@gmail.com>
2019-10-21 11:29:02 -0400

    Add empty AdminUserPage.js component

---
+++ frontend/petlabsfrontend/src/Components/AdminUserPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class AdminUserPage extends React.Component {
+    render() {
+        return(
+            <div>AdminUserPage</div>
+        );
+    }
+}
+
+export default AdminUserPage;
