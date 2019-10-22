423164dc9408fd3d54a3a1d406cd798725e92e71
Lily <lilyhgxiao@gmail.com>
2019-10-22 19:26:11 -0400

    Added User Views to app.js

--- frontend/petlabsfrontend/src/App.js
+++ frontend/petlabsfrontend/src/App.js
@@ -6,15 +6,20 @@ import AdminDashboardPage from './Components/AdminDashboardPage';
 import AdminUserListPage from './Components/AdminUserListPage';
 import AdminPetListPage from './Components/AdminPetListPage';
 import AdminItemListPage from './Components/AdminItemListPage';
+import UserDashboardPage from './Components/UserDashboardPage';
+import LogInPage from './Components/LogInPage';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={AdminDashboardPage} />
+        <Route path="/" exact component={LogInPage} />
+        <Route path="/AdminDashboardPage" exact component={AdminDashboardPage} />
         <Route path="/AdminUserListPage" component={AdminUserListPage} />
         <Route path="/AdminPetListPage" component={AdminPetListPage} />
         <Route path="/AdminItemListPage" component={AdminItemListPage} />
+
+        <Route path="/UserDashboardPage" exact component={UserDashboardPage} />
       </Switch>
     </Router>
   );
