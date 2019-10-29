ac2133a6b156ac89990fff4b43a8e1954a035172
cherkaski <alexcher81@gmail.com>
2019-10-29 10:41:17 -0400

    Complete AdminItemPage

--- frontend/petlabsfrontend/src/Components/AdminItemPage.js
+++ frontend/petlabsfrontend/src/Components/AdminItemPage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Database from '../TempClasses/Database';
 import AdminSideMenu from '../Components/AdminSideMenu';
 import '../CSS/ItemView.css';
-// import AddNew from '../Images/add_new.png';
 import { Link } from 'react-router-dom';
 import saveIcon from '../Images/Save_Icon.png';
 
@@ -22,45 +21,45 @@ class AdminItemPage extends React.Component {
     getItemReference() {
         for (let i = 0; i < Database.itemList.length; i++) {
             if (Database.itemList[i].name === this.props.location.itemName) {
-                console.log(Database.itemList[i]);
                 return Database.itemList[i];
             }
         }
     }
     getTableRows() {
+        let key = 0;
         const result = [];
         const headerRow = (
-            <tr className={'item-view'}>
+            <tr key={key++} className={'item-view'}>
                 <th className={'item-view'}>Properties</th>
                 <th className={'item-view'}>Values</th>
             </tr>
         );
         const strength = (
-            <tr className={'item-view'}>
+            <tr key={key++} className={'item-view'}>
                 <td className={'item-view'}>Strength Rate</td>
                 <td className={'item-view'}><input className={'row'} type='Text' value={this.state.strength} onChange={this.handleStrengthChange} /></td>
             </tr>
         );
         const speed = (
-            <tr className={'item-view'}>
+            <tr key={key++} className={'item-view'}>
                 <td className={'item-view'}>Speed Rate</td>
                 <td className={'item-view'}><input className={'row'} type='Text' value={this.state.speed} onChange={this.handleSpeedChange} /></td>
             </tr>
         );
         const intelligence = (
-            <tr className={'item-view'}>
+            <tr key={key++} className={'item-view'}>
                 <td className={'item-view'}>Intelligence Rate</td>
                 <td className={'item-view'}><input className={'row'} type='Text' value={this.state.intelligence} onChange={this.handleIntelligenceChange} /></td>
             </tr>
         );
         const happiness = (
-            <tr className={'item-view'}>
+            <tr key={key++} className={'item-view'}>
                 <td className={'item-view'}>Happiness Rate</td>
                 <td className={'item-view'}><input className={'row'} type='Text' value={this.state.happiness} onChange={this.handleHappinessChange} /></td>
             </tr>
         );
         const fullness = (
-            <tr className={'item-view'}>
+            <tr key={key++} className={'item-view'}>
                 <td className={'item-view'}>Fullness Rate</td>
                 <td className={'item-view'}><input className={'row'} type='Text' value={this.state.fullness} onChange={this.handleFullnessChange} /></td>
             </tr>
@@ -91,18 +90,32 @@ class AdminItemPage extends React.Component {
     handleFullnessChange = (event) => {
         this.setState({fullness: (parseInt(event.target.value) ? parseInt(event.target.value) : 0)});
     }
+
+    /**
+     * Update the properties of the reference
+     */
+    handleSaveClick = () => {
+        console.log(this.item);
+        this.item.name = this.state.name;
+        this.item.strength = this.state.strength
+        this.item.speed = this.state.speed
+        this.item.intelligence = this.state.intelligence;
+        this.item.happiness = this.state.happiness;
+        this.item.fullness = this.state.fullness;
+        console.log(this.item);
+    }
     render() {
         return (
         <div>
             <Link to={'./AdminDashboardPage'}>
-                <img className={'saveIcon'} src={saveIcon} alt={'Save Icon'}></img>
+                <img className={'saveIcon'} src={saveIcon} alt={'Save Icon'} onClick={this.handleSaveClick}></img>
             </Link>
             <AdminSideMenu />
                 <div className='main'>
                     <div className='mainForm'>
                         <h1>Item ID: {this.getItemReference().id}</h1>
                         <div className={'centerView'}>
-                            <p>Name: <input className={'addItemLink'} type='Text' value={this.state.name} onChange={this.handleNameChange}/> </p> 
+                            <p className={'addItemLink'}>Name: <input className={'addItemLink'} type='Text' value={this.state.name} onChange={this.handleNameChange}/> </p> 
                             <p className={'centerLeft'}>Sprite:</p>
                             <img className={'addItemLink'} src={this.getItemReference().imgURL} alt={'Add new Image'} />
                         </div>
@@ -112,10 +125,6 @@ class AdminItemPage extends React.Component {
                                 {this.getTableRows()}
                             </tbody>
                         </table>
-                        {/* <br />
-                        <div className={'centerView'}>
-                            <button className={'addItemLink'}>Save</button>
-                        </div> */}
                     </div>
                 </div>
         </div>
