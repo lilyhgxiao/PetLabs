7efc84cfb86237a990c877626f4f511803ae37fc
LKhyeon <lkdh97@gmail.com>
2019-10-30 00:09:36 -0400

    edit user PC so it uses DB

--- frontend/petlabsfrontend/src/Components/UserPetCarePage.js
+++ frontend/petlabsfrontend/src/Components/UserPetCarePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import '../CSS/__.css';
+
 import '../CSS/UserPetCareStyles.css';
 import PetCareAction from './PetCareAction.js';
 import PetModel from './PetModel.js';
@@ -7,8 +7,8 @@ import PetStatus from './PetStatus.js';
 import UserSideMenu from './UserSideMenu';
 
 import Pet from '../TempClasses/Pet';
+import mockDB from '../TempClasses/Database';
 
-// Images
 import petHappy from '../Images/pet_happy_placeholder.png';
 import petNeutral from '../Images/pet_neutral_placeholder.png';
 import petSad from '../Images/pet_sad_placeholder.png';
@@ -18,30 +18,13 @@ const log = console.log
 class UserPetCarePage extends React.Component {
 
     state = {
-        petName: "",
-        petImg: petNeutral,
-        fullness: 50,
-        happiness: 50,
-        intelligence: 0,
-        strength: 0,
-        speed: 0,
-        alive: true
+        petObj : [],
+        petImg: petNeutral
     }
 
     componentDidMount() {
-        const pet = this.props.location.state.pet;
-
-        this.setState({
-            petName: pet.petName,
-            petImg: petNeutral,
-            fullness: pet.hunger,
-            happiness: pet.happiness,
-            intelligence: pet.intelligence,
-            strength: pet.strength,
-            speed: pet.speed,
-            alive: pet.alive
-        })
-
+        this.findPet()
+        this.setStartImage()
         this.dTimer = setInterval(
             () => this.starve(),
         1000
@@ -52,63 +35,85 @@ class UserPetCarePage extends React.Component {
         clearInterval(this.dTimer)
     }
 
-    starve() {
-        if (this.state.alive) {
+    // Find specific pet from the database:
+    findPet() {
+        const petReceived = this.props.location.state.pet;
+        const pList = mockDB.petList;
+        let i = 0;
+        while (i < pList.length) {
+            if (pList[i].petName == petReceived.petName) {
+                this.setState({
+                    petObj: pList[i]
+                })
+                i += pList.length;
+            } else {
+                i++;
+            }
+        }
+    }
+
+    // Set starting image accordingly.          
+    setStartImage() {
+        if (this.state.petObj.happiness < 20) {
             this.setState({
-                fullness: this.state.fullness - 2
+                petImg: petSad
             })
-            this.fatigue()
+        } else if (this.state.petObj.happiness > 90) {
+            this.setState({
+                petImg: petHappy
+            })
+        }
+    }
+
+    starve() {
+        if (this.state.petObj.alive) {
+            this.state.petObj.hunger -= 2;
+            this.fatigue();
         }
     }
 
     fatigue() {
-        if (this.state.fullness < 20 && this.state.fullness >= -20) {
-            this.setState({
-                happiness: this.state.happiness - 20
-            })
-            if (this.state.happiness < 20) {
+        if (this.state.petObj.hunger < 20 && this.state.petObj.hunger >= -20) {
+
+            this.state.petObj.happiness -= 20;
+            
+            if (this.state.petObj.happiness < 20) {
                 this.setState({
                     petImg: petSad
                 })
             }
-        } if (this.state.fullness < -20) {
-            this.setState({
-                alive: false
-            })
+        } if (this.state.petObj.hunger< -20) {
+            this.state.petObj.alive = false;
         }
     }
 
     // Function related to feeding.
     feedPet = () => {
         log('feeding: -10 hunger');
-        if (this.state.alive) {
-            this.setState({
-                fullness: this.state.fullness + 10
-            })
+        if (this.state.petObj.alive) {
+            this.state.petObj.hunger += 10;
         }
     }
 
     // Function related to feeding.
     playWithPet = () => {
         log('playing with pet: +2 happiness');
-        if (this.state.alive) {
+        if (this.state.petObj.alive) {
 
             let incValue = 2;
-            if (this.state.fullness < 20 && this.state.fullness >= -20) {
+            if (this.state.petObj.hunger < 20 && this.state.petObj.hunger >= -20) {
                 incValue = 1;
-            } else if (this.state.fullness < -20) {
+            } else if (this.state.petObj.hunger < -20) {
                 incValue = 0;
             }
 
-            this.setState({
-                happiness: this.state.happiness + incValue
-            })
+            this.state.petObj.happiness += incValue;
             
-            if (this.state.happiness > 60 && this.state.happiness <= 90) {
+            if (this.state.petObj.happiness > 60 && this.state.petObj.happiness <= 90) {
                 this.setState({
                     petImg: petNeutral
                 })
-            } else if (this.state.happiness > 90) {
+            } else if (this.state.petObj.happiness > 90) {
                 this.setState({
                     petImg: petHappy
                 })
@@ -118,14 +123,12 @@ class UserPetCarePage extends React.Component {
 
     // Function related to feeding.
     trainPet = () => {
-        if (this.state.alive) {
+        if (this.state.petObj.alive) {
             log('training pet: +1 all stats');
-            this.setState({
-                fullness: this.state.fullness - 6,
-                intelligence: this.state.intelligence + 1,
-                strength: this.state.strength + 1,
-                speed: this.state.speed + 1
-            })
+            this.state.petObj.hunger -= 6;
+            this.state.petObj.intelligence += 1;
+            this.state.petObj.strength += 1;
+            this.state.petObj.speed += 1;
             this.fatigue()
         } 
     }
@@ -137,17 +140,17 @@ class UserPetCarePage extends React.Component {
                 <div className='main'>
                     { /* Shows status of the pet */ }  
                     <PetStatus
-                        numFullness = {this.state.fullness}
-                        numHappiness = {this.state.happiness}
-                        numIntelligence = {this.state.intelligence}
-                        numStrength = {this.state.strength}
-                        numSpeed = {this.state.speed}
+                        numFullness = {this.state.petObj.hunger}
+                        numHappiness = {this.state.petObj.happiness}
+                        numIntelligence = {this.state.petObj.intelligence}
+                        numStrength = {this.state.petObj.strength}
+                        numSpeed = {this.state.petObj.speed}
                     />
                     
                     { /* Shows model of the pet with name */ }  
                     <PetModel
                         imgSource = {this.state.petImg}
-                        petName = {this.state.petName}
+                        petName = {this.state.petObj.petName}
                     />
 
                     { /* A table that contains three buttons */ }  
