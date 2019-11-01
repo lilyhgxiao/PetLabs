384e692bded3df67233d00d1d40d0e717fc69751
LKhyeon <lkdh97@gmail.com>
2019-10-31 22:04:51 -0400

    Added support for viewing pet info

--- frontend/petlabsfrontend/src/Components/AdminUserPage.js
+++ frontend/petlabsfrontend/src/Components/AdminUserPage.js
@@ -8,6 +8,7 @@ import '../CSS/ItemView.css';
 
 class AdminUserPage extends React.Component {
 	targetUserName = this.props.location.username;
+	petChanges = [];
 
 	state = {
         username: this.targetUserName,
@@ -21,7 +22,7 @@ class AdminUserPage extends React.Component {
 
 	componentDidMount() {
         this.findUserInfo();
-        // this.populatePets();
+        this.populatePets();
     }
 
 	findUserInfo() {
@@ -44,30 +45,128 @@ class AdminUserPage extends React.Component {
 		}
 	}
 
-  //   populatePets() {
-		// let targetUser = Database.userList[this.state.userInd];
-  //       let pEntries = document.querySelector("#petEntries");
-  //       let pList = targetUser.petIdList;
-  //       	console.log(pEntries)
-  //       	console.log(pList)
-  //       for (let i = 0; pList.length; i++) {
-  //       	let idText = document.createTextNode("Pet id: " + pList[i])
-  //       	pEntries.appendChild(idText);
+    populatePets() {
+		let targetUser = Database.userList[this.state.userInd];
+        let pEntries = document.querySelector("#petEntries");
+        let pidList = targetUser.petIdList;
+        for (let i = 0; i < pidList.length; i++) {
+        	// Create new div element:
+        	let nDiv = document.createElement('div');
 
-  //       }
-  //   }
+        	// Put pet's id:
+        	let pId = document.createTextNode("Pet id: " + pidList[i])
+        	nDiv.appendChild(pId);
+
+        	// Create a table:
+        	let nTable = document.createElement('table');
+        	let tbodyPart = document.createElement('tbody');
+        	nTable.setAttribute('class', 'item-view');
+        	
+        	// Start populating:
+        	let pList = Database.petList;
+        	let j = 0;
+        	while (j < pList.length) {
+        		if (pList[j].id == pidList[i]) {
+        			let innerArray = [];
+        			innerArray.push(pList[j].id);
+
+        			// Add Pet Name entry:
+		        	let pNameReturn = this.AddPetTR("Pet Name", 
+		        							pList[j].petName, this.handlePetNameChange, pidList[i]);
+		        	tbodyPart.appendChild(pNameReturn);
+		        	innerArray.push(pList[j].petName);
+		        	
+		        	// Add Pet Hunger entry:
+		        	let pHungerReturn = this.AddPetTR("Pet Hunger", 
+		        							pList[j].hunger, this.handlePetHungerChange, pidList[i]);
+		        	tbodyPart.appendChild(pHungerReturn);
+		        	innerArray.push(pList[j].hunger);
+
+		        	// Add Pet Happniess entry:
+		        	let pHappinessReturn = this.AddPetTR("Pet Happniess", 
+		        							pList[j].happiness, this.handlePetHappinessChange, pidList[i]);
+		        	tbodyPart.appendChild(pHappinessReturn);
+		        	innerArray.push(pList[j].happiness);
+
+		        	// Add Pet Intelligence entry:
+		        	let pIntelligenceReturn = this.AddPetTR("Pet Intelligence", 
+		        							pList[j].intelligence, this.handlePetIntelligenceChange, pidList[i]);
+		        	tbodyPart.appendChild(pIntelligenceReturn);
+		        	innerArray.push(pList[j].intelligence);
+
+		        	// Add Pet Strength entry:
+		        	let pStrengthReturn = this.AddPetTR("Pet Strength", 
+		        							pList[j].strength, this.handlePetStrengthChange, pidList[i]);
+		        	tbodyPart.appendChild(pStrengthReturn);
+		        	innerArray.push(pList[j].strength);
+
+		        	// Add Pet Speed entry:
+		        	let pSpeedReturn = this.AddPetTR("Pet Speed", 
+		        							pList[j].speed, this.handlePetSpeedChange, pidList[i]);
+		        	tbodyPart.appendChild(pSpeedReturn);
+		        	innerArray.push(pList[j].speed);
+		        	
+		        	this.petChanges.push(innerArray);
+        			j += pList.length;
+        		}
+        		j++;
+        	}
+        	nTable.appendChild(tbodyPart);
+        	nDiv.appendChild(nTable);
+        	pEntries.appendChild(nDiv);
+
+        }
+    }
+
+    AddPetTR(atrName, atrValue, handler, index) {
+    	// Create new tr:
+		let trEntry = document.createElement('tr');
+		trEntry.setAttribute('class', 'item-view');
+
+		// Create td for attribute name:
+		let atrN = document.createElement('td');
+		atrN.setAttribute('class', 'item-view');
+
+    	let atrTxt = document.createTextNode(atrName)
+    	atrN.appendChild(atrTxt);
+    	trEntry.appendChild(atrN);
+
+    	// Create td for the field:
+    	let nInput = document.createElement('input');
+    	nInput.setAttribute('class', 'addItemLink');
+    	nInput.setAttribute('type', 'Text');
+    	nInput.setAttribute('value', atrValue);
+    	nInput.setAttribute('petid', index)
+    	nInput.onchange = handler;
+
+    	trEntry.appendChild(nInput);
+    	return trEntry;
+    }
 
     /* Event Handlers */
 
 	handleSaveClick = () => {
 		let targetUser = Database.userList[this.state.userInd];
-        console.log(this.petType);
         targetUser.name = this.state.username;
         targetUser.strengthRate = this.state.password;
         targetUser.speedRate = this.state.isAdmin;
         targetUser.intelligenceRate = this.state.petIdList;
         targetUser.happinessRate = this.state.itemIdList;
         targetUser.happinessRate = this.state.gold;
+
+    	let pList = Database.petList;
+        for (let i = 0; i < this.petChanges.length; i++) {
+        	for (let j = 0; j < pList.length; j++) {
+        		if (this.petChanges[i][0] == pList[j].id) {
+        			pList[j].petName = this.petChanges[i][1];
+        			pList[j].hunger = this.petChanges[i][2];
+        			pList[j].happiness = this.petChanges[i][3];
+        			pList[j].intelligence = this.petChanges[i][4];
+        			pList[j].strength = this.petChanges[i][5];
+        			pList[j].speed = this.petChanges[i][6];
+        		}
+        	}
+        }
     }
 
     handlePWChange = (e) => {
@@ -76,8 +175,63 @@ class AdminUserPage extends React.Component {
         });
     }
 
+    /* For pet changes events */
+
+    handlePetNameChange = (e) => {
+    	let petId = e.target.attributes.getNamedItem('petid').value;
+    	for (let i = 0; i < this.petChanges.length; i++) {
+    		if (this.petChanges[i][0] == petId) {
+    			this.petChanges[i][1] = e.target.value;
+    		}
+    	}
+    }
+
+    handlePetHungerChange = (e) => {
+        let petId = e.target.attributes.getNamedItem('petid').value;
+    	for (let i = 0; i < this.petChanges.length; i++) {
+    		if (this.petChanges[i][0] == petId) {
+    			this.petChanges[i][2] = e.target.value;
+    		}
+    	}
+    }
+
+    handlePetHappinessChange = (e) => {
+        let petId = e.target.attributes.getNamedItem('petid').value;
+    	for (let i = 0; i < this.petChanges.length; i++) {
+    		if (this.petChanges[i][0] == petId) {
+    			this.petChanges[i][3] = e.target.value;
+    		}
+    	}
+    }
+
+    handlePetIntelligenceChange = (e) => {
+        let petId = e.target.attributes.getNamedItem('petid').value;
+    	for (let i = 0; i < this.petChanges.length; i++) {
+    		if (this.petChanges[i][0] == petId) {
+    			this.petChanges[i][4] = e.target.value;
+    		}
+    	}
+    }
+
+    handlePetStrengthChange = (e) => {
+        let petId = e.target.attributes.getNamedItem('petid').value;
+    	for (let i = 0; i < this.petChanges.length; i++) {
+    		if (this.petChanges[i][0] == petId) {
+    			this.petChanges[i][5] = e.target.value;
+    		}
+    	}
+    }
+
+    handlePetSpeedChange = (e) => {
+        let petId = e.target.attributes.getNamedItem('petid').value;
+    	for (let i = 0; i < this.petChanges.length; i++) {
+    		if (this.petChanges[i][0] == petId) {
+    			this.petChanges[i][6] = e.target.value;
+    		}
+    	}
+    }
+
     render() {
-        console.log(this.props);
         return(
             <div>
                 <Link to={'./AdminDashboardPage'}>
@@ -93,10 +247,9 @@ class AdminUserPage extends React.Component {
 
                 <div className='main'>
 	                <div className='mainForm'>
-	                    <h1>Username: {this.state.username}</h1>
+	                    <h1>Id: {this.state.username}</h1>
 	                    <table className={'item-view'}>
                             <tbody>
-
                                 <tr className={'item-view'}>
 						            <td className={'item-view'}>
 						            	Password
@@ -124,18 +277,12 @@ class AdminUserPage extends React.Component {
 				                    	/>
 						            </td>
 						        </tr>
-
-						        <tr className={'item-view'}>
-						            <td className={'item-view'}>
-						            	Pets
-						            </td>
-						            <td className={'item-view'} id="petEntries">
-						            	
-						            </td>
-						        </tr>
-
                             </tbody>
                         </table>
+                        <h2>List of pets</h2>
+                        <div id='petEntries'>
+
+                        </div>
 	                </div>
 	            </div>
             </div>
