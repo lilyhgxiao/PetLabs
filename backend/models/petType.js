0860cc0d96599d9f7061560405c44ccf18881da0
cherkaski <alexcher81@gmail.com>
2019-11-23 14:57:51 -0500

    Add Pet mongoose model and express routes

--- backend/models/petType.js
+++ backend/models/petType.js
@@ -1,14 +1,3 @@
-// this.name = name;
-// this.neutralImage = neutralImage;
-// this.happyImage = happyImage;
-// this.sadImage = sadImage;
-// this.strengthRate = strengthRate;
-// this.speedRate = speedRate;
-// this.intelligenceRate = intelligenceRate;
-// this.fullnessRate = fullnessRate;
-// this.happinessRate = happinessRate;
-// this.price = price;
-
 // User mongoose model
 const mongoose = require('mongoose');
 
--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const { mongoose } = require('./db/mongoose');
 // import mongoose models
 const { Item } = require('./models/item');
 const { PetType } = require('./models/petType');
+const { Pet } = require('./models/pet');
 
 // Get module that validates ObjectID's for mongodb.
 const { ObjectID } = require('mongodb');
@@ -231,6 +232,112 @@ app.delete('/pettypes/:id', (request, response) => {
     })
 })
 
+// PET ROUTES
+
+// POST route to create a new petType.
+app.post('/pets', (request, response) => {
+    // Create a new petType using the mongoose petType model.
+    const pet = new Pet({
+        ownerName: request.body.ownerName,
+        petName: request.body.petName,
+        type: request.body.type,
+        // neutralImage: ,
+        // happyImage: ,
+        // sadImage: ,
+        strength: request.body.strength,
+        speed: request.body.speedRate,
+        intelligence: request.body.intelligence,
+        happiness: request.body.happiness,
+        fullness: request.body.fullness,
+        price: request.body.price
+    });
+
+    // Save the petType to the database.
+    pet.save().then((result) => {
+        response.status(200).send(result);
+    }, (error) => {
+        response.status(400).send(error);
+    });
+});
+
+// GET route to get all pets
+app.get('/pets', (request, response) => {
+    Pet.find().then((result) => {
+        response.status(200).send(result);
+    }, (error) => {
+        response.status(500).send(error);
+    });
+});
+
+// GET route to get a pet with a specific id
+app.get('/pets/:id', (request, response) => {
+    // Extract the id from the URL wildcard
+    const id = request.params.id;
+
+    // Validate the id to ensure it is a valid mongodb id.
+    if (!ObjectID.isValid(id)) {
+        response.status(404).send();
+    }
+
+    // Find the pet by ID
+    Pet.findById(id).then((result) => {
+        if (!result) {
+            response.status(404).send();
+        } else {
+            response.status(200).send(result)
+        }
+    },).catch((error) => {
+        response.status(500).send(error);
+    });
+});
+
+// PATCH route to update an petType
+app.patch('/pets/:id', (request, response) => {
+    // Get the petType id from the URL
+    const id = request.params.id;
+
+    // Check for a valid mongodb id
+    if (!ObjectID.isValid(id)) {
+        response.status(404).send();
+    }
+
+    // Get the object with properties to update.
+    const update = getPetPropertiesToUpdate(request);
+
+    // Attempt to update the item with the specified id.
+    Pet.findByIdAndUpdate(id, { $set: update }, { new: true }).then((result) => {
+        if (!result) {
+            response.status(404).send();
+        } else {
+            response.status(200).send(result);
+        }
+    }).catch((error) => {
+        response.status(500).send(error);
+    });
+});
+
+// DELETE route to delete a pet.
+app.delete('/pets/:id', (request, response) => {
+    // Get the id from the from the URL
+    const id = request.params.id;
+
+    // Check for a valid mongodb id
+    if (!ObjectID.isValid(id)) {
+        response.status(404).send();
+    }
+
+    // Attempt to remove the pet with the specefied id
+    Pet.findByIdAndRemove(id).then((result) => {
+        if (!result) {
+            response.status(404).send();
+        } else {
+            response.status(200).send(result);
+        }
+    }).catch((error) => {
+        response.status(500).send(error);
+    })
+})
+
 // HELPER FUNCTIONS
 
 // Helper function that returns an object with all the properties to be update in the item.
@@ -239,12 +346,15 @@ function getItemPropertiesToUpdate(request) {
 
     if (request.body) {
         if (request.body.name) update.name = request.body.name;
+
         if (request.body.strength) update.strength = request.body.strength;
         if (request.body.speed) update.speed = request.body.speed;
         if (request.body.intelligence) update.intelligence = request.body.intelligence;
         if (request.body.happiness) update.happiness = request.body.happiness;
         if (request.body.fullness) update.fullness = request.body.fullness;
+
         if (request.body.imgURL) update.imgURL = request.body.imgURL;
+
         if (request.body.price) update.price = request.body.price;
     }
 
@@ -256,15 +366,38 @@ function getPetTypePropertiesToUpdate(request) {
 
     if (request.body) {
         if (request.body.name) update.name = request.body.name;
+
         if (request.body.neutralImage) update.neutralImage = request.body.neutralImage;
         if (request.body.happyImage) update.happyImage = request.body.happyImage;
         if (request.body.sadImage) update.sadImage = request.body.sadImage;
+
         if (request.body.strengthRate) update.strengthRate = request.body.strengthRate;
         if (request.body.speedRate) update.speedRate = request.body.speedRate;
         if (request.body.intelligenceRate) update.intelligenceRate = request.body.intelligenceRate;
         if (request.body.happinessRate) update.happinessRate = request.body.happinessRate;
         if (request.body.fullnessRate) update.fullnessRate = request.body.fullnessRate;
+
         if (request.body.price) update.price = request.body.price;
+    }
+
+    return update;
+}
+
+function getPetPropertiesToUpdate(request) {
+    const update = {};
+
+    if (request.body) {
+        if (request.body.ownerName) update.ownerName = request.body.ownerName;
+        if (request.body.petName) update.petName = request.body.petName;
+        if (request.body.type) update.type = request.body.type;
+
+        if (request.body.strength) update.strength = request.body.strength;
+        if (request.body.speed) update.speed = request.body.speed;
+        if (request.body.intelligence) update.intelligence = request.body.intelligence;
+        if (request.body.happiness) update.happiness = request.body.happiness;
+        if (request.body.fullness) update.fullness = request.body.fullness;
+        
+        if (request.body.alive) update.alive = request.body.alive;
     }
 
     return update;
